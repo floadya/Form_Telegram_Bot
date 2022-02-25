a0e6b151b09da5ab8668b9b560c2bd47e71dbd47
floadya <94679106+floadya@users.noreply.github.com>
2022-02-25 18:27:13 +0700

    Update CONFIG.py

--- CONFIG.py
+++ CONFIG.py
@@ -1,6 +1,6 @@
 # Insert bot token here
-api_token = "BOT_TOKEN"
+api_token = "5222563883:AAGPnOA9iCfCn53F7Htz3ke9vJ4Gmhdwgfs"
 
 
 # Replace admin_link with your username (https://t.me/username)
-admin_link = "https://t.me/12345" 
+admin_link = "https://t.me/dedisut" 
